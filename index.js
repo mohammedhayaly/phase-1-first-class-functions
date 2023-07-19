e533edde6ad9a9781615963630036d47e8eb90a1
mohammedhayaly <mohammed.alhayali88@yahoo.com>
2023-07-20 00:47:11 +0300

    done

---
+++ index.js
@@ -0,0 +1,25 @@
+
+
+function receivesAFunction(callback) {
+  callback(); 
+}
+
+function returnsANamedFunction() {
+  function namedFunction() {
+    console.log("I am a named function!");
+  }
+
+  return namedFunction; 
+}
+
+function returnsAnAnonymousFunction() {
+  return function () {
+    console.log("I am an anonymous function!");
+  };
+}
+
+module.exports = {
+  receivesAFunction,
+  returnsANamedFunction,
+  returnsAnAnonymousFunction,
+};
